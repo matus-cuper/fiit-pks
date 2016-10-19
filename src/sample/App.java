9adcaa3c86385412f5842569ca7828f08954d9c3
32cupo <matus.cuper@gmail.com>
2016-10-19 16:06:14 +0200

    Added platform exit just in case app runs after JavaFx termination

--- src/sample/App.java
+++ src/sample/App.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -19,5 +20,6 @@ public class App extends Application {
 
     public static void main(String[] args) {
         launch(args);
+        Platform.exit();
     }
 }
