58e96b4db1fb0221add50a27dc85e221a4e0379e
32cupo <matus.cuper@gmail.com>
2016-10-19 12:51:13 +0200

    Added CRC32 for testing purposes

---
+++ src/model/fragment/MyChecksum.java
@@ -0,0 +1,40 @@
+package model.fragment;
+
+import java.nio.ByteBuffer;
+import java.util.zip.CRC32;
+import java.util.zip.Checksum;
+
+/**
+ * Created by Matus Cuper on 11.9.2016.
+ *
+ * MyChecksum class represents CRC checksum of each sent fragment.
+ * Provides data verification of received packets
+ */
+public class MyChecksum {
+
+    private byte[] checksum;
+
+    public MyChecksum(byte[] data) {
+        this.computeChecksum(data);
+    }
+
+    private void computeChecksum(byte[] data) {
+        Checksum checksum = new CRC32();
+        checksum.update(data, 0, data.length);
+        this.checksum = this.longToBytes(checksum.getValue());
+    }
+
+    private byte[] longToBytes(long l) {
+        ByteBuffer buffer = ByteBuffer.allocate(Integer.BYTES);
+        buffer.putInt((int) l);
+        return buffer.array();
+    }
+
+    byte[] getChecksum() {
+        return checksum;
+    }
+
+    public void setChecksum(byte[] checksum) {
+        this.checksum = checksum;
+    }
+}
